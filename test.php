ff0fa51f0f221acb08100fe7775c110390f6fcfc
lzjtuxzs <947703573@qq.com>
2016-03-18 20:34:40 +0800

    修改文档的注释信息

--- test.php
+++ test.php
@@ -1,9 +1,15 @@
 <?php
-
+/**
+ *  测试文件--记录一些项目会用到的代码片段，并进行测试
+ *  Author: Luo Ning <luoning@luoning.me>
+ *  Date: 2016/3/18 20:33
+ *  The code for static-blog.
+ */
 
 /**
  *  使用生成器高效迭代处理大型数据集
- * @
+ *  @param String $filename
+ *  @return Iterator
  */
 function FileLineGenerator($file) {
     if(!$fh = fopen($file, 'r')) {
@@ -14,3 +20,51 @@ function FileLineGenerator($file) {
     }
     fclose($fh);
 }
+
+/**
+ *  根据文件读取的每行，确定一篇文章的一些元数据 meta
+ *  $param Iterator $file
+ *  @return Array
+ */
+function getAttrArr(Iterator $file)
+{
+	$line_num = 0;
+	$attr_arr = [];
+	foreach($file as $line) {
+	    $line_num++;
+	    // echo $line_num;
+		if ($line_num > 1 && trim($line) == '---') {
+			break;
+		}
+		// echo $line;
+
+		if ($line_num > 1) {
+
+			$temp = explode(':', trim($line));
+
+			$attr_str = trim($temp[1]);
+
+			if (count($temp) > 2) {
+				
+				foreach ($temp as $k => $v) {
+					if ($k < 2) 
+						continue;
+
+					$attr_str .= ':' . $v;
+				}
+
+			}
+			$attr_arr[$temp[0]] = $attr_str;
+		}
+	}
+
+	return $attr_arr;
+}
+
+//Test to use
+
+$file = FileLineGenerator('post.md');
+
+$attr_arr = getAttrArr($file);
+
+print_r($attr_arr);
