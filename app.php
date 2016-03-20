b8e996267531ca1c0db82c715f3caea9dd840aea
lzjtuxzs <947703573@qq.com>
2016-03-20 13:52:24 +0800

    测试yaml

--- app.php
+++ app.php
@@ -3,9 +3,30 @@
 require 'vendor/autoload.php';
 
 use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Yaml\ParseException;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Dumper;
 
-$yaml = new Parser();
+// $yaml = new Parser();
 
-$value = $yaml->parse(file_get_contents('config.yml'));
+// try {
+// 	$value = $yaml->parse(file_get_contents('config.yml'));
+// } catch (ParseException $e) {
+// 	 printf("wwwUnable to parse the YAML string: %s", $e->getMessage());
+// }
+// var_dump( $value );
 
-var_dump( $value );
+$yaml = Yaml::parse(file_get_contents('config.yml'));
+
+var_dump( $yaml );
+
+// $array = array(
+//     'foo' => 'bar',
+//     'bar' => array('foo' => 'bar', 'bar' => 'baz'),
+// );
+
+// $dumper = new Dumper();
+
+// $yaml = $dumper->dump($array,2);
+
+// file_put_contents('yaml.yml', $yaml);
