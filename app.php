b519a89a4fe86737d7559e243cfc314d174919d2
lzjtuxzs <947703573@qq.com>
2016-03-22 22:55:50 +0800

    test console

--- app.php
+++ app.php
@@ -6,10 +6,17 @@ use Symfony\Component\Yaml\Parser;
 use Symfony\Component\Yaml\ParseException;
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Yaml\Dumper;
+
 use Stb\Command\GreetCommand;
+use Symfony\Component\Console\Application;
 
 
-$greet = new GreetCommand();
+// $greet = new GreetCommand();
+
+
+$application = new Application();
+$application->add(new GreetCommand());
+$application->run();
 
 // var_dump($greet);
 
