551484b17c8b68d992a577ce4ac4e99a1addca56
Luo Ning <947703573@qq.com>
2016-03-26 22:35:12 +0800

    nothing

--- app.php
+++ app.php
@@ -4,33 +4,6 @@ require 'vendor/autoload.php';
 $commands = require_once __DIR__ . '/bootstrap/console.php';
 $application = require_once __DIR__ . '/bootstrap/app.php';
 
-$application->addCommands(
-    $commands
-);
+$application->addCommands($commands);
 
 $application->run();
-
-
-// $yaml = new Parser();
-
-// try {
-// 	$value = $yaml->parse(file_get_contents('config.yml'));
-// } catch (ParseException $e) {
-// 	 printf("wwwUnable to parse the YAML string: %s", $e->getMessage());
-// }
-// var_dump( $value );
-
-// $yaml = Yaml::parse(file_get_contents('config.yml'));
-
-// var_dump( $yaml );
-
-// $array = array(
-//     'foo' => 'bar',
-//     'bar' => array('foo' => 'bar', 'bar' => 'baz'),
-// );
-
-// $dumper = new Dumper();
-
-// $yaml = $dumper->dump($array,2);
-
-// file_put_contents('yaml.yml', $yaml);
