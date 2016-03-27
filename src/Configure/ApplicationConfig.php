d9e714ac162c2848d6058155a3b57805851a9317
Luo Ning <947703573@qq.com>
2016-03-27 18:45:25 +0800

    add yaml and filesystem componments

---
+++ src/Configure/ApplicationConfig.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Stb\Configure;
+
+use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+
+/**
+ * Load the application Configure.
+ */
+class ApplicationConfig
+{
+
+    function __construct()
+    {
+        # code...
+    }
+}
