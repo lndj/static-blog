7d600294a38dd0924c5596f62ad5c62f1e0696ee
Luo Ning <947703573@qq.com>
2016-03-27 18:54:07 +0800

    application config

--- src/Configure/ApplicationConfig.php
+++ src/Configure/ApplicationConfig.php
@@ -5,6 +5,7 @@ namespace Stb\Configure;
 use Symfony\Component\Yaml\Parser;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Load the application Configure.
@@ -12,8 +13,26 @@ use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 class ApplicationConfig
 {
 
-    function __construct()
+    private $config_path = __DIR__ . '/../../config.yml';
+
+    public $parser;
+
+    function __construct(Parser $parser, $config_path = '')
     {
-        # code...
+        if ($config_path != '') {
+            $this->config_path = $config_path;
+        }
+        $this->parser = $parser;
+    }
+
+    public function getByKey($key)
+    {
+        try {
+            $config = $this->parser->parse(file_get_contents($this->config_path));
+        } catch (Exception $e) {
+            echo '1ssss';
+        }
+
+        return $config[$key];
     }
 }
