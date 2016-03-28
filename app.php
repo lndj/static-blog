ee684766b67bf0c3af6189f5fc90df106a3b1ed0
Luo Ning <947703573@qq.com>
2016-03-28 23:28:43 +0800

    添加注释

--- app.php
+++ app.php
@@ -4,6 +4,7 @@ require 'vendor/autoload.php';
 $commands = require_once __DIR__ . '/bootstrap/console.php';
 $application = require_once __DIR__ . '/bootstrap/app.php';
 
+//Add the commands to the application.
 $application->addCommands($commands);
-
+//Run it
 $application->run();
