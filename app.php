9d8f38224c9b60584e3a74a580a174c426c4cb6a
Luo Ning <947703573@qq.com>
2016-03-31 23:41:07 +0800

    test process

--- app.php
+++ app.php
@@ -1,10 +1,23 @@
 <?php
 
 require 'vendor/autoload.php';
-$commands = require_once __DIR__ . '/bootstrap/console.php';
-$application = require_once __DIR__ . '/bootstrap/app.php';
+// $commands = require_once __DIR__ . '/bootstrap/console.php';
+// $application = require_once __DIR__ . '/bootstrap/app.php';
+//
+// //Add the commands to the application.
+// $application->addCommands($commands);
+// //Run it
+// $application->run();
 
-//Add the commands to the application.
-$application->addCommands($commands);
-//Run it
-$application->run();
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+$process = new Process('php test.php');
+$process->run();
+
+// executes after the command finishes
+if (!$process->isSuccessful()) {
+    throw new ProcessFailedException($process);
+}
+
+echo $process->getOutput();
