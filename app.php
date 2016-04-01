66e803d7054a6c9861c0a3d82b4865d526ac6ae5
Luo Ning <947703573@qq.com>
2016-04-01 23:41:40 +0800

    test 2 process

--- app.php
+++ app.php
@@ -16,10 +16,21 @@ $process = new Process('php test.php');
 
 $process->start();
 
+$p = new Process('php test.php');
+$p->start();
+
 echo 'qqqq';
 sleep(1);
 
 $process->wait(function ($type, $buffer) {
+    if (Process::ERR === $type) {
+        echo 'ERR > '.$buffer;
+    } else {
+        echo 'OUT > '.$buffer;
+    }
+});
+
+$p->wait(function ($type, $buffer) {
     if (Process::ERR === $type) {
         echo 'ERR > '.$buffer;
     } else {
